3c09e57ff97911658b2e24d36cdecbd57a5eefa3
phieu37 <86352994+phieu37@users.noreply.github.com>
2024-03-06 19:39:23 +0700

    100.Xây dựng UI phần Header #5

--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -4,11 +4,10 @@ import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
 
-function MenuItem({ data }) {
-    // p25 đang lỗi render
+function MenuItem({ data, onClick }) {
     // return <button>{JSON.stringify(data)}</button>;
     return (
-        <Button className={cx('menu-item')} leftIcon={data.icon} to={data.to}>
+        <Button className={cx('menu-item')} leftIcon={data.icon} to={data.to} onClick={onClick}>
             {data.title}
         </Button>
     );
