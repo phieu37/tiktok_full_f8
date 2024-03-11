008bbb72014d4f56a249301aac402cf418840aff
phieu37 <86352994+phieu37@users.noreply.github.com>
2024-03-11 16:46:00 +0700

    107. Xây dựng UI phần Header #7

---
+++ src/components/Image/index.js
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+import { forwardRef, useState } from 'react';
+import images from '~/assets/images';
+import styles from './Image.module.scss';
+
+// ref do forwardRef đưa xuống
+// khi component Image được mounted vào nó sẽ bê ref của img forward ra ngoài cho
+// Image thì Tippy sẽ nhận được
+// đổi tên fallback thành customFallback để tránh trùng tên với fallback bên trong useState
+// hoặc sửa fallback trong useState thành _fallback
+// Khi ko truyền fallback từ ngoài vào sẽ lấy: images.noImage, khi có truyền fallback từ ngoài 
+// vào thì images.noImage ko được sử dụng mà lấy từ bên ngoài
+const Image = forwardRef(({ src, alt, className, fallback: customFallback = images.noImage, ...props }, ref) => {
+    // xử lý nếu ảnh lỗi thì lấy ảnh lỗi
+    const [fallback, setFallback] = useState('');
+    const handleError = () => {
+        setFallback(customFallback);
+    };
+
+    // styles.wrapper là file scss của chính nó Image.module.scss -> chung
+    // custom css class riêng sẽ vào className -> riêng
+    return (
+        <img
+            className={classNames(styles.wrapper, className)}
+            ref={ref}
+            src={fallback || src}
+            alt={alt}
+            {...props}
+            onError={handleError}
+        />
+    );
+});
+
+export default Image;
