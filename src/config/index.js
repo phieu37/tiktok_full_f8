b9e933a9b6eb51c844c6c281c9a24c9f3867c1cd
phieu37 <86352994+phieu37@users.noreply.github.com>
2024-03-14 18:07:12 +0700

    114.Tái cấu trúc và tối ưu code #1

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+// Làm thế này để gộp tất cả config vào 1 cái cho gọn khi cần gọi ra cho dễ
+import routes from './routes';
+
+const config = {
+  routes
+};
+
+export default config;
--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -3,16 +3,18 @@
 // custom thêm post put path
 import axios from 'axios';
 
-const request = axios.create({
-    baseURL: 'https://tiktok.fullstack.edu.vn/api/',
+console.log(process.env);
+
+const httpRequest = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 // tối ưu để xử lý vđề setSearchResult(res.data.data); // data 1 của axios, data 2 của Api trả về
 // options = {} ko bắt buộc nên để rỗng -> là param
 // path là users/search
 export const get = async (path, options = {}) => {
-  const respone = await request.get(path, options)
+  const respone = await httpRequest.get(path, options)
   return respone.data
 }
 
-export default request;
+export default httpRequest;
